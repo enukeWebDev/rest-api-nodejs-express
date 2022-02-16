04618bb16259cee0b3e00d77f43058de44cab8c4
Roderick <enuke4@gmail.com>
2022-02-16 13:30:46 -0600

    test patch using postman - no issue

--- routes/users.js
+++ routes/users.js
@@ -66,7 +66,24 @@ router.delete('/:id', (req, res) => {
 router.patch('/:id', (req, res) => {
   const { id } = req.params;
 
+  const { firstName, lastName, age } = req.body;
+
   const user = users.find((user) => user.id === id)
+
+
+  if (firstName) {
+    user.firstName = firstName;
+  }
+
+  if (lastName) {
+    user.lastName = lastName;
+  }
+
+  if (age) {
+    user.age = age;
+  }
+
+  res.send(`User with ID ${id} has been update.`)
 });
 
 export default router;
