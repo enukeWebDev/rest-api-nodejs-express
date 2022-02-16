ac4423e4245c7c8ec036237ec1c98f64494bd1e6
Roderick <enuke4@gmail.com>
2022-02-16 11:18:00 -0600

    server set up

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+//Initialize the express application
+const app = express();
+
+const PORT = 5000;
+
+//Initialize body-parser middleware
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`Listening to PORT - ${PORT}`));
+
+
