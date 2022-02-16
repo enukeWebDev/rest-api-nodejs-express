f682ee7ba013d43bc9281f46a2b7055c6b967546
Roderick <enuke4@gmail.com>
2022-02-16 13:10:07 -0600

    test delete route in postman - no issue

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
-import route from 'color-convert/route';
+// import route from 'color-convert/route';
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 
 //Initialize Router
 const router = express.Router();
 
-const users = [
+let users = [
   // {
   //   firstName: "Erick",
   //   lastName: "Nuke",
@@ -40,27 +40,27 @@ router.post('/', (req, res) => {
   // console.log(req.body);
   users.push(userWithId);
 
-  //This will send message to the users
+  //   //This will send message to the users
   res.send(`New user with the name ${user.firstName} ${user.lastName} is added.`);
 
 });
 
 //: means can expext anything
-// router.get('/:id', (req, res) => {
+router.get('/:id', (req, res) => {
 
-//   const { id } = req.params;
+  const { id } = req.params;
 
-//   const foundUser = users.find((user) => user.id === id)
-//   res.send(foundUser);
-// })
+  const foundUser = users.find((user) => user.id === id)
+  res.send(foundUser);
+})
 
-// route.delete('./:id', (req, res) => {
-//   const { id } = req.params;
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
 
-//   users = users.filter((user) => user.id !== id);
+  users = users.filter((user) => user.id !== id);
 
-//   res.send(`User with ID ${id} has been deleted.`);
+  res.send(`User with ID ${id} has been deleted.`);
 
-// })
+})
 
 export default router;
